2c230a4917ecb9493878b3cf53c435ac70632d79
travis <90806701+travisscott232@users.noreply.github.com>
2022-01-04 20:22:23 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+function Execute() {
+  document.title = "Classes";
+
+  document.getElementById("change").innerHTML = "Tab Name changed";
+}
+
+
+
+Execute()
+
+
+function Execute2() {
+  document.title = "Google Docs";
+
+  document.getElementById("change").innerHTML = "Tab Name changed";
+}
+
+
+
+Execute2()
+
+function inspect() {
+  document.body.contentEditable = true; void 0;
+}
+
+
+inspect()
